cd2f4218e68afda5267a0b94954a39febe161c86
DawsonGeist <Dawson.geist@gmail.com>
2017-04-18 02:05:28 -0400

    Add files via upload

--- main.java
+++ main.java
@@ -1,78 +1,111 @@
-
+import java.util.*;
 public class main 
 {
 	
-	public static int[] generateList(int n)
+	public static ArrayList<Integer> generateList(int n)
 	{
-		int[] list = new int[n];
+		ArrayList<Integer> list = new ArrayList<Integer>();
 		for(int i = 0; i < n; i++)//iterates through list
 		{
-			list[i] = (int)(10*Math.random());//generates 0-9
+			list.add((int)(10*Math.random()));//generates 0-9
 		}
 		return list;
 	}
 	
-	//if a is > b, return true and switch
-	public static boolean isLarger(int a, int b) 
-	{
-		//a is k-1, b is k 
-		if(a>b)
-		return true;
-		else
-		return false;
-	}
 	
-	public static int[] sort(int[] returner)
+	public static ArrayList<Integer> sort(ArrayList<Integer> n)
 	{
-		int temp;
-		int sortingBubble = returner.length;
-		//goes through entire list, shortens by 1 every iteration
-		for(int i = sortingBubble;i>0;i--)
+		int length = n.size();
+		ArrayList<Integer> returner = n;
+		int largerNum;
+		
+		//bubble through list, bubble decreases by one
+		for(int k = length; k > 0; k--)
 		{
-			//loops through list comparing k and k-1
-			for(int k = 1; k<returner.length; k++)
+			
+			/*
+			 * Pulls the "Switching" logic down the list until it 
+			 * reaches the end of the "Sort Bubble" (k)
+			 */
+			
+			for(int i = 1; i < k; i++)//Bounded by Sorting Bubble
 			{
-				//switching logic
-				if(isLarger(k-1,k))
+				/*
+				 * returner: a,b,c... 
+				 * b = i, a = i-1;
+				 * if a > b
+				 * "Switch"
+				 * returner: b,a,c...
+				 */
+				//if()
 				{
-					temp = returner[k-1];
-					returner[k-1] = returner[k];
-					returner[k] = temp;
+					//implement truth statement
+					//sort
 				}
 			}
-		}	
+			
+		}
+		
+		
+		
 		return returner;
 	}
 	
 	
 	public static void main(String[] args)
 	{
-		System.out.println("Welcome to Bubble Sort 1.0\n");
-		int[] list = new int[10];
-		list = generateList(10);
+	
+//		System.out.println("Welcome to Bubble Sort 1.0\n");
+//		int[] list = new int[10];
+//		list = generateList(10);
+//		
+//		for(int i = 0; i < 10; i++)
+//		{
+//			if(i==9)
+//			{
+//				System.out.print(list[i]+"\n");
+//			}
+//			else
+//			System.out.print(list[i] + ", ");
+//		}
+//		
+//		System.out.println("\n\tSorted list:\n");
+//		
+//		sort(list);
+//		
+//		for(int i = 0; i < 10; i++)
+//		{
+//			if(i==9)
+//			{
+//				System.out.print(list[i]+"\n");
+//			}
+//			else
+//			System.out.print(list[i] + ", ");
+//		}
 		
-		for(int i = 0; i < 10; i++)
+		System.out.println("Testing the switch algorithym");
+		ArrayList<Integer> test = new ArrayList<Integer>();
+		//print out list
+		test.add(5);
+		test.add(3);
+		for(int i = 0; i<test.size();i++)
 		{
-			if(i==9)
-			{
-				System.out.print(list[i]+"\n");
-			}
-			else
-			System.out.print(list[i] + ", ");
+			System.out.print(test.get(i) + " ");
 		}
-		
-		System.out.println("\n\tSorted list:\n");
-		
-		sort(list);
-		
-		for(int i = 0; i < 10; i++)
+		//switch the first two
+		int largerNum;
+		if(test.get(1)<test.get(0))
 		{
-			if(i==9)
-			{
-				System.out.print(list[i]+"\n");
-			}
-			else
-			System.out.print(list[i] + ", ");
+			System.out.print("True");
+			largerNum = test.get(0);
+			test.set(0,test.get(1));
+			test.set(1, largerNum);
+		}
+		//print out updated list
+		System.out.println("Updated:");
+		for(int i = 0; i<test.size();i++)
+		{
+			System.out.print(test.get(i) + " ");
 		}
 		
 	}
